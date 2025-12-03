583cc12faf0b92ac2f5c33c2cc4fd6e1c42700c2
Alfredo <ajc7873@gmail.com>
2025-12-02 20:46:42 -0700

    Agregue la logica al ServidorCoup

--- src/servidor/ServidorCoup.java
+++ src/servidor/ServidorCoup.java
@@ -1,8 +1,27 @@
 package servidor;
 
-import javax.script.Bindings;
+import comun.Constantes;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 
 public class ServidorCoup {
     public static ArrayList<HiloCliente> clientesConectados = new ArrayList<>();
+    public static void main(String[] args) {
+        System.out.println(">> Iniciando Servidor Coup Shavalon...");
+        try (ServerSocket serverSocket = new ServerSocket(Constantes.PUERTO)) {
+            System.out.println(">> Servidor esperando jugadores en puerto: " + Constantes.PUERTO);
+
+            while (true) {
+                Socket socketCliente = serverSocket.accept();
+                System.out.println(">> ¡Nuevo cliente conectado!: " + socketCliente.getInetAddress());
+                HiloCliente nuevoHilo = new HiloCliente(socketCliente);
+                clientesConectados.add(nuevoHilo);
+                nuevoHilo.start();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
