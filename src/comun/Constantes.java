c3e254b2e85b013723ca9ac2c20566a3198b4f35
Alfredo <ajc7873@gmail.com>
2025-12-02 20:25:19 -0700

    Agregue Constantes.java en donde estaran las variables que nunca van a cambiar y defini el nomnre exacto para cada rol

---
+++ src/comun/Constantes.java
@@ -0,0 +1,28 @@
+package comun;
+
+public class Constantes {
+    public static final int PUERTO = 8080;
+
+    // Tipos de Mensajes
+    public static final String TIPO_LOGIN = "LOGIN";         // del cliente al Servidor
+    public static final String TIPO_MENSAJE = "TEXTO";       // mensajes de sistema o chat
+    public static final String TIPO_ESTADO = "ESTADO";       // del servidor al cliente
+    public static final String TIPO_ACCION = "ACCION";       // del cliente al servidor
+    public static final String TIPO_TURNO = "TURNO";         // del servidor  cliente
+
+    // Roles del Juego
+    public static final String DUQUE = "Duque";
+    public static final String ASESINO = "Asesino";
+    public static final String CAPITAN = "Capitan";
+    public static final String EMBAJADOR = "Embajador";
+    public static final String CONDESA = "Condesa";
+
+    // Acciones
+    public static final String ACCION_INGRESOS = "Toma una";       // 1 moneda
+    public static final String ACCION_AYUDA = "Toma dos";    // 2 monedas
+    public static final String ACCION_IMPUESTOS = "Toma tres";     // 3 monedas (Duque)
+    public static final String ACCION_ASESINAR = "Asesinar";       // -3 monedas (Asesino)
+    public static final String ACCION_ROBAR = "Robar";             // Robar 2 (Capitán)
+    public static final String ACCION_CAMBIO = "Cambio";           // Cambiar cartas (Embajador)
+    public static final String ACCION_GOLPE = "Coup";   // 7 monedas
+}
