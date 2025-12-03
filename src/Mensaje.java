59e283862ed20a58d24c130a11a099c65efb5bb9
Alfredo <ajc7873@gmail.com>
2025-12-02 20:06:04 -0700

    Agregue variables tipo y contenido, ademas que cree el constructor principal

--- src/Mensaje.java
+++ src/Mensaje.java
@@ -1,2 +1,15 @@
-public class Mensaje {
+import java.io.Serializable;
+
+public class Mensaje implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public String tipo;
+    public Object contenido;
+
+    public Mensaje() {
+    }
+    public Mensaje(String tipo, Object contenido){
+        this.tipo = tipo;
+        this.contenido = contenido;
+    }
 }
