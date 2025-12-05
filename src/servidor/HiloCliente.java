b46f9a55444acbbd9e0a113c10911372fc8775ff
Alfredo <ajc7873@gmail.com>
2025-12-05 10:44:28 -0700

    Modifique el run en HiloCliente.java para manejar mejor cuando un usuario se desconecta

--- src/servidor/HiloCliente.java
+++ src/servidor/HiloCliente.java
@@ -9,10 +9,6 @@ import java.net.Socket;
 
 public class HiloCliente extends Thread {
 
-
-public String getNombreJugador() { 
-    return nombreJugador;
-}
     private Socket socket;
     private ObjectOutputStream salida;
     private ObjectInputStream entrada;
@@ -22,46 +18,57 @@ public String getNombreJugador() {
         this.socket = socket;
     }
 
+    public String getNombreJugador() {
+        return nombreJugador;
+    }
+
     @Override
     public void run() {
         try {
             salida = new ObjectOutputStream(socket.getOutputStream());
             salida.flush();
             entrada = new ObjectInputStream(socket.getInputStream());
-          while (true) {
-    Mensaje msjRecibido = (Mensaje) entrada.readObject();
+            while (true) {
+                Mensaje msjRecibido = (Mensaje) entrada.readObject();
 
-    if (msjRecibido.tipo.equals(Constantes.TIPO_LOGIN)) {
-      
-        this.nombreJugador = (String) msjRecibido.contenido;
-        
-     
-        enviarMensaje(new Mensaje(Constantes.TIPO_ESTADO, "Conectado como " + nombreJugador));
-
-       
-        ServidorCoup.enviarATodos(new Mensaje(Constantes.TIPO_MENSAJE, ">> " + nombreJugador + " se ha unido a la sala."));
-        
-        System.out.println("Login procesado para: " + nombreJugador);
-    } 
-    else {
-   
-        System.out.println("Mensaje recibido: " + msjRecibido.tipo);
-    }
-}
-
-        } catch (Exception e) {
-            System.out.println(">> Cliente desconectado.");
+                if (msjRecibido.tipo.equals(Constantes.TIPO_LOGIN)) {
+                    this.nombreJugador = (String) msjRecibido.contenido;
+                    enviarMensaje(new Mensaje(Constantes.TIPO_ESTADO, ">> Conectado exitosamente como: " + nombreJugador));
+                    ServidorCoup.enviarATodos(new Mensaje(Constantes.TIPO_MENSAJE, ">> El jugador " + nombreJugador + " ha entrado al lobby."));
+                    System.out.println("Login procesado: " + nombreJugador);
+                }
+                else if (msjRecibido.tipo.equals(Constantes.TIPO_MENSAJE)) {
+                    ServidorCoup.enviarATodos(new Mensaje(Constantes.TIPO_MENSAJE, nombreJugador + ": " + msjRecibido.contenido));
+                }
+                else {
+                    System.out.println("Comando desconocido de " + nombreJugador + ": " + msjRecibido.tipo);
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println(">> Cliente " + (nombreJugador != null ? nombreJugador : "Desconocido") + " se ha desconectado.");
         } finally {
-            try { socket.close(); } catch (IOException ex) {}
-            ServidorCoup.clientesConectados.remove(this);
+            desconectar();
         }
     }
+
     public void enviarMensaje(Mensaje msj) {
         try {
             salida.writeObject(msj);
             salida.flush();
         } catch (IOException e) {
-            System.out.println("Error enviando mensaje a cliente.");
+            System.out.println("Error enviando mensaje a " + nombreJugador);
+        }
+    }
+
+    private void desconectar() {
+        try {
+            ServidorCoup.clientesConectados.remove(this);
+            if(nombreJugador != null) {
+                ServidorCoup.enviarATodos(new Mensaje(Constantes.TIPO_MENSAJE, ">> " + nombreJugador + " ha abandonado el servidor."));
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
