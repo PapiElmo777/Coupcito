ad79477329fb825bd273bec86c8f210c8872dc7c
Enrique <proenrique4@gmail.com>
2025-12-06 00:17:52 -0600

    Atributos para definir la sala

--- src/servidor/Sala.java
+++ src/servidor/Sala.java
@@ -1,5 +1,19 @@
 package servidor;
 
-public class Sala {
-    
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sala implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static int contadorIds = 1;
+
+    private int id;
+    private int capacidadMaxima;
+    private boolean esPrivada;
+    private boolean enJuego;
+    private String nombreAdmin;
+
+   
+   
 }
