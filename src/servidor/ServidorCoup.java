569fbc8a18ac6ecb63ee5474c7d79b0d6cde6978
Enrique <proenrique4@gmail.com>
2025-12-08 00:56:01 -0600

    Metodo de gestion de salas

--- src/servidor/ServidorCoup.java
+++ src/servidor/ServidorCoup.java
@@ -6,16 +6,16 @@ import comun.Mensaje;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ServidorCoup {
+
     
-public static void enviarATodos(Mensaje msj) {
-    for (HiloCliente cliente : clientesConectados) {
-      
-        cliente.enviarMensaje(msj);
-    }
-}
     public static ArrayList<HiloCliente> clientesConectados = new ArrayList<>();
+    
+
+    public static List<Sala> salasActivas = new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println(">> Iniciando Servidor Coup Shavalon...");
         BaseDatos.conectar();
@@ -26,7 +26,7 @@ public static void enviarATodos(Mensaje msj) {
                 Socket socketCliente = serverSocket.accept();
                 System.out.println(">> ¡Nuevo cliente conectado!: " + socketCliente.getInetAddress());
                 HiloCliente nuevoHilo = new HiloCliente(socketCliente);
-                clientesConectados.add(nuevoHilo);
+              
                 nuevoHilo.start();
             }
 
@@ -34,9 +34,45 @@ public static void enviarATodos(Mensaje msj) {
             e.printStackTrace();
         }
     }
+
     public static void broadcast(Mensaje msj) {
         for (HiloCliente cliente : clientesConectados) {
             cliente.enviarMensaje(msj);
         }
     }
+
+    // --- MÉTODOS DE GESTION DE SALAS 
+
+    public static synchronized Sala crearSala(HiloCliente creador, int capacidad, boolean privada) {
+        Sala nuevaSala = new Sala(creador, capacidad, privada);
+        salasActivas.add(nuevaSala);
+        System.out.println(">> Sala creada ID " + nuevaSala.getId() + " por " + creador.getNombreJugador());
+        return nuevaSala;
+    }
+
+    public static synchronized Sala buscarSala(int id) {
+        for (Sala s : salasActivas) {
+            if (s.getId() == id) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    public static synchronized String obtenerListaSalasPublicas() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("--- SALAS DISPONIBLES ---\n");
+        boolean haySalas = false;
+        for (Sala s : salasActivas) {
+            if (!s.isEsPrivada() && !s.isEnJuego()) {
+                sb.append(s.toString()).append("\n");
+                haySalas = true;
+            }
+        }
+        if (!haySalas) sb.append("No hay salas públicas disponibles.\n");
+        sb.append("-------------------------");
+        return sb.toString();
+    }
+    
+
 }
