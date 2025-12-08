1a9ce69f81a14b892ca7914f389d035680587c6c
Alfredo <ajc7873@gmail.com>
2025-12-08 08:03:28 -0700

    Añandi el costructor del gestor de salas, cree un metodo de acceso y el metodo para crear una sala

--- src/servidor/GestorSalas.java
+++ src/servidor/GestorSalas.java
@@ -1,4 +1,25 @@
 package servidor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GestorSalas {
+    private static GestorSalas instancia;
+    private List<Sala> salas;
+
+    private GestorSalas() {
+        this.salas = new ArrayList<>();
+    }
+
+    public static synchronized GestorSalas getInstancia() {
+        if (instancia == null) {
+            instancia = new GestorSalas();
+        }
+        return instancia;
+    }
+    public Sala crearSala(HiloCliente creador, int capacidad, boolean privada) {
+        Sala nueva = new Sala(creador, capacidad, privada);
+        salas.add(nueva);
+        return nueva;
+    }
 }
